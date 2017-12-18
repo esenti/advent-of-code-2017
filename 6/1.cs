89e7ce18295405c6f33b8df693c014c42816674a
Krzysztof Skoracki <krzysztof@skoracki.net>
2017-12-18 21:20:30 +0100

    Add day 6

---
+++ 6/1.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Aoc
+{
+    class Day6
+    {
+        static void BalanceBank(int[] banks, int index) {
+            int v = banks[index];
+            banks[index] = 0;
+
+            while(v-- > 0) {
+                index++;
+
+                banks[index % banks.Length]++;
+            }
+
+        }
+
+        static void Main()
+        {
+
+            string content = File.ReadAllText("input.txt");
+            Console.WriteLine(content);
+            int[] banks = Array.ConvertAll(content.Split('\t'), int.Parse);
+
+            List<int> seenValues = new List<int>();
+
+            int step = 0;
+
+            while(step++ > -1) {
+                int index = Array.IndexOf(banks, banks.Max());
+                BalanceBank(banks, index);
+
+                int hash = string.Join(", ", banks).GetHashCode();
+
+                if(seenValues.Contains(hash)) {
+                    break;
+                }
+
+                seenValues.Add(hash);
+
+                Console.WriteLine(string.Join(", ", banks));
+            }
+
+            Console.WriteLine(step);
+        }
+    }
+}
